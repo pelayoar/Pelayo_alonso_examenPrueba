46fc7bccd712f5174a1ccc854f3b29ddc77bfe65
pelayoar <pelayoar@gmail.com>
2021-11-08 17:37:15 +0000

    WAP: Examen prueba practicar v1

---
+++ app/src/main/java/com/example/alonso_pelayo_examen1t/EnviarActivity.kt
@@ -0,0 +1,27 @@
+package com.example.alonso_pelayo_examen1t
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import com.example.alonso_pelayo_examen1t.databinding.ActivityEnviarBinding
+import com.example.alonso_pelayo_examen1t.databinding.ActivityMainBinding
+
+class EnviarActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityEnviarBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityEnviarBinding.inflate(layoutInflater)
+
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.eliminar.setOnClickListener {
+            binding.eliminar.isVisible = false
+            binding.textView.isVisible = false
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/alonso_pelayo_examen1t/Registrar.kt
@@ -0,0 +1,11 @@
+package com.example.alonso_pelayo_examen1t
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registrar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrar)
+    }
+}
